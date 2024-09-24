6875dfbdd78b006239e9eae6a370c471786d629f
Niko <nikolaus.kapfer.rupp@gmail.com>
2024-09-24 13:09:06 +0200

    added array exercises

---
+++ lesson_1/letter_frequency.js
@@ -0,0 +1,5 @@
+let statement = "The Flintstones Rock";
+let letterObj = {};
+let statmentArray = statement.split();
+
+//statmentArray.forEach((letter) => letterObj[letter] = (statmentArray.filter((filterLetter)) => filterletter === letter))
